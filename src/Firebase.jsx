4ef65be6d0bd2f86ca794b0fa7c6a7e4028c274c
Sefa Yeşilarslan <110385923+sefayesilarslan@users.noreply.github.com>
2025-06-20 14:41:03 +0300

    Update Firebase.jsx

--- src/Firebase.jsx
+++ src/Firebase.jsx
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "###########################",
-  authDomain: "#####################",
-  projectId: "####################",
-  storageBucket: "###########################",
-  messagingSenderId: "###########################",
-  appId: "###########################",
+  apiKey: "###########################", // Firebaseden alınan bilgiler eklenecek.
+  authDomain: "#####################", // Firebaseden alınan bilgiler eklenecek.
+  projectId: "####################", // Firebaseden alınan bilgiler eklenecek.
+  storageBucket: "###########################", // Firebaseden alınan bilgiler eklenecek.
+  messagingSenderId: "###########################", // Firebaseden alınan bilgiler eklenecek.
+  appId: "###########################", // Firebaseden alınan bilgiler eklenecek.
 };
 
 export const app = initializeApp(firebaseConfig);
